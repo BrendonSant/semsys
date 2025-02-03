f8abebd06aec3341dfedc0c6440229fa92bb8c89
BRENDON M. C. SANTOS <52220998+BrendonSant@users.noreply.github.com>
2025-02-03 10:45:09 -0300

    add image hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import heroImg from "../assets/hero.svg"
 
 export default function Home() {
   return (
-    <main>
-      <h1>Teste</h1>
+    <main className="flex items-center flex-col justify-center min-h-screen">
+      <h1>Gerencie sua empresa</h1>
+      <h1>Atendimentos, clientes</h1>
+      <Image
+      src={heroImg}
+      alt='Imagem hero do dev controle'
+      width={600}
+      className="max-w-sm md:max-w-xl"
+      />
     </main>
   );
 }
