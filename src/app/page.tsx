39ee6c0f37a41b3bcda301bf69349bbe21decc00
BRENDON M. C. SANTOS <52220998+BrendonSant@users.noreply.github.com>
2025-02-04 18:02:00 -0300

    ajuste pagina de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,43 @@
 import Image from "next/image";
-import heroImg from "../assets/hero.svg";
+import heroImg from "../assets/technology.png";
+import { LogoSys } from "@/icons/logosys";
+import { Logo } from "@/icons/logo";
 
 export default function Home() {
   return (
     <main className="flex items-center flex-col justify-center min-h-[calc(100vh-80px)] ">
     
-      <div className="flex items-center gap-10 flex-col lg:flex-row lg:justify-around  justify-center w-full lg:max-w-screen-2xl">
-        <div>
+      <div className="flex items-center gap-10 flex-col lg:flex-row lg:justify-between  justify-center w-full ">
+        <div className="flex flex-col items-center justify-center w-1/2">
         <h1 className="font-bold text-3xl md:text-4xl text-blue-500">
-        Gerencie sua empresa
+        Faça seu cadastro ou login
       </h1>
       <h3 className="font-semibold text-xl md:text-4xl">
+        Atendimentos, clientes
+      </h3>
+        </div>
+        <div className=" flex flex-col items-center justify-around h-[calc(100vh-80px)] w-1/2 bg-gradient-to-b from-[#ffffff] to-[#072e5a] ">
+        <LogoSys />
+        <div className="flex flex-col justify-center items-center">
+        <h1 className="font-bold text-3xl md:text-4xl text-white">
+        Gerencie sua empresa
+      </h1>
+      <h3 className="font-semibold text-xl md:text-2xl  text-white">
         Atendimentos, clientes
       </h3>
         </div>
         <Image
           src={heroImg}
           alt="Imagem hero do dev controle"
-          width={600}
+          width={400}
           className="max-w-sm md:max-w-xl"
         />
+        <div className="flex flex-col text-white">
+          <Logo/>
+          <span>santsmcb</span>
+        </div>
+        </div>
+       
       </div>
     </main>
   );
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,11 +3,11 @@ import {FiUser,FiLogOut} from "react-icons/fi"
 
 export function Header(){
     return(
-        <header className="w-full flex itens-center px-2 py-4 bg-white h-20 shadow-sm">
-            <div className="w-full flex itens-center justify-between max-w-7xl mx-auto">
+        <header className="w-full flex itens-center px-2 py-4 bg-white h-20 shadow-sm ">
+            <div className="w-full flex my-auto itens-center justify-between max-w-7xl mx-auto">
                 <Link href="/">
                 <h1 className="font font-bold text-2xl pl-1 hover:tracking-widest">
-                    <span className="text-blue-500">DEV</span> CONTROLE
+                    semsys
                 </h1>
                 </Link>
                 
