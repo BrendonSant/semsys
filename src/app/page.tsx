4a8a9bffd6b2f4df44e326581033f2d010c9542d
BRENDON M. C. SANTOS <52220998+BrendonSant@users.noreply.github.com>
2025-02-05 14:34:06 -0300

    login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import heroImg from "../assets/technology.png";
+import heroImg from "../assets/grafico-de-linha-unscreen.gif";
 import { LogoSys } from "@/icons/logosys";
 import { Logo } from "@/icons/logo";
 import Aurora from "@/components/animations/background/aurora";
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="flex items-center flex-col justify-center min-h-screen ">
       <div className="flex items-center gap-10 flex-col lg:flex-row lg:justify-between  justify-center w-full ">
-        <div className="flex flex-col items-center justify-around h-[calc(100vh-80px)] w-1/3">
+        <div className="flex flex-col items-center justify-around h-[calc(100vh-80px)] w-1/2">
           <div className="flex flex-col justify-center items-center">
             <LogoSys />
             <h1 className="font font-bold text-2xl pl-1 hover:tracking-widest text-mdblue-500">
@@ -16,8 +16,16 @@ export default function Home() {
             </h1>
           </div>
           <div className="flex flex-col items-center justify-start w-full h-1/2">
-            <div className="flex flex-col items-center justify-center w-full ">
-              <h1 className="font-bold text-3xl md:text-4xl text-mgray-300">
+            <div className="flex flex-col items-center justify-center  w-full ">
+              <div className="flex flex-col justify-center items-center mb-8">
+                <h1 className="font-bold text-3xl md:text-4xl text-mdblue-500">
+                  Gerencie sua empresa
+                </h1>
+                <h3 className="font-semibold text-xl md:text-2xl text-mdblue-500">
+                  Atendimentos e clientes
+                </h3>
+              </div>
+              <h1 className="font-bold text-3xl md:text-2xl text-mgray-300">
                 Faça seu cadastro ou login
               </h1>
               <h3 className="font-bold text-xl md:text-xl text-mgray-300">
@@ -30,30 +38,19 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="relative flex flex-col items-center justify-around h-screen w-2/3">
+        <div className="relative flex flex-col items-center justify-around h-screen w-1/2">
           {/* Aurora */}
-          <Aurora
-            colors={["#ffffff", "#ffffff", "#ffffff"]}
-            speed={0.8}
-            
-          />
+          <Aurora colors={["#ffffff", "#ffffff", "#ffffff"]} speed={0.8} />
 
           {/* Conteúdo principal */}
           <div className=" absolute flex flex-col justify-around h-screen items-center z-10">
             <LogoSys />
-            <div className="flex flex-col justify-center items-center">
-              <h1 className="font-bold text-3xl md:text-4xl text-white">
-                Gerencie sua empresa
-              </h1>
-              <h3 className="font-semibold text-xl md:text-2xl text-white">
-                Atendimentos e clientes
-              </h3>
-            </div>
+
             <Image
               src={heroImg}
               alt="Imagem hero do dev controle"
-              width={300}
-              className="max-w-sm md:max-w-xl opacity-15"
+              width={250}
+              className="max-w-sm md:max-w-xl opacity-30"
             />
             <div className="flex flex-col text-white">
               <Logo />
