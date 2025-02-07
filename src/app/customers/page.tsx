19a52b0f4e027443550e04f39149ae276e1ad680
BRENDON M. C. SANTOS <52220998+BrendonSant@users.noreply.github.com>
2025-02-07 13:52:35 -0300

    page

--- src/app/customers/page.tsx
+++ src/app/customers/page.tsx
@@ -6,7 +6,7 @@ import { FiUserPlus } from "react-icons/fi";
 export default function Customers(){
     return(
         <Container>
-             <div className="flex w-full mt-10 mb-4">
+             <div className="flex w-full justify-between  mt-10 mb-4">
                <h1 className="text-3xl font-bold font-sans text-mdblue-500">Clientes</h1>
                <Button variant="outline">
                 <FiUserPlus/>
