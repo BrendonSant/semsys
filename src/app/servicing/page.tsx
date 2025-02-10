d3c7438ece9d09ff5102a85447bbf0b5a177df7d
Brendon <brendonmcs@hotmail.com>
2025-02-09 23:52:55 -0300

    criada a pagina newservicing

--- src/app/servicing/page.tsx
+++ src/app/servicing/page.tsx
@@ -4,13 +4,9 @@ import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import prismaClient from "@/lib/prisma";
 import { FiEdit, FiTrash, FiUserPlus } from "react-icons/fi";
-import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-} from "@/components/ui/sheet";
+
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Servicing() {
   const session = await getServerSession(authOptions);
@@ -37,88 +33,15 @@ export default async function Servicing() {
         <h1 className="text-3xl font-bold font-sans text-mdblue-500">
           Serviços
         </h1>
-        <Sheet>
-          <Button variant={"outline"}>
+        <Link href='servicing/newservicing'>
+        <Button variant={"outline"}>
             <FiUserPlus />
             Novo serviço
           </Button>
 
-          <SheetContent className="w-full md:w-1/2 ">
-            <SheetHeader>
-              <SheetTitle className="font-montserrat font-bold text-3xl text-mdblue-500">
-                Lançar serviço
-              </SheetTitle>
-            </SheetHeader>
-            <div className="mt-10">
-              <div className="w-full flex flex-col">
-                <form className="w-full flex flex-col">
-                  <label className="mb-1 font-medium text-lg" htmlFor="">
-                    Titulo do Serviço
-                  </label>
-                  <input
-                    className="w-full border border-gray-300 px-2 rounded-md h-11 mb-2"
-                    placeholder="Digie o titulo do serviço"
-                    type="text"
-                    required
-                  />
-                  <label className="mb-1 font-medium text-lg" htmlFor="">
-                    Selecione o cliente
-                  </label>
-                  <select className="w-full border border-gray-300 px-2 rounded-md h-11 mb-2">
-                    {customers.map((customers) => (
-                      <option key={customers.id} value={customers.id}>
-                        {customers.name}
-                      </option>
-                    ))}
-                  </select>
+        </Link>
+        
 
-                  <label className="mb-1 font-medium text-lg" htmlFor="">
-                    Valor do serviço
-                  </label>
-                  <input
-                    className="w-full border border-gray-300 px-2 rounded-md h-11 mb-2"
-                    placeholder="Digie o titulo do serviço"
-                    type="text"
-                    required
-                  />
-
-                  <label className="mb-1 font-medium text-lg" htmlFor="">
-                    Descrição
-                  </label>
-                  <textarea
-                    className="w-full border border-gray-300 px-2 rounded-md h-24 mb-2"
-                    placeholder="Descreva o serviço"
-                    required
-                  ></textarea>
-
-                  <label className="mb-1 font-medium text-lg" htmlFor="">
-                    Selecione o status pagamento
-                  </label>
-                  <select
-                    defaultValue={"Pendente"}
-                    className="w-full border border-gray-300 px-2 rounded-md h-11 mb-2"
-                  >
-                    <option value="Realizado">Realizado</option>
-                    <option value="Pendente">Pendente</option>
-                  </select>
-
-                  <label className="mb-1 font-medium text-lg" htmlFor="">
-                    Selecione o status
-                  </label>
-                  <select
-                    defaultValue={"Não iniciado"}
-                    className="w-full border border-gray-300 px-2 rounded-md h-11 mb-2"
-                  >
-                    <option value="Realizado">Realizado</option>
-                    <option value="Parado">Parado</option>
-                    <option value="Execultando">Execultando</option>
-                    <option value="Não iniciado">Não iniciado</option>
-                  </select>
-                </form>
-              </div>
-            </div>
-          </SheetContent>
-        </Sheet>
       </div>
       <table className="min-w-full my-2 ">
         <thead className=" my-2 border border-gray-200 h-8">
