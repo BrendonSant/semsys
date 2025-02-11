faab8eb6cf1733cbdc2a16386732f55c3c90e0b4
Brendon <brendonmcs@hotmail.com>
2025-02-11 00:47:17 -0300

    update

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -12,7 +12,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import prismaClient from "@/lib/prisma";
-import { ChartOverview } from "../chart";
+
 
 
 export async function TabDashboard() {
