7252f9a4f11c16194e051beb240867882e803434
BRENDON M. C. SANTOS <52220998+BrendonSant@users.noreply.github.com>
2025-02-11 21:35:00 -0300

    update service

--- src/app/servicing/page.tsx
+++ src/app/servicing/page.tsx
@@ -49,8 +49,8 @@ export default async function Servicing() {
             <th className="  hidden lg:block">Data</th>
             <th className="mx-1">Valor</th>
             <th className="mx-1">Total</th>
-            <th className="  hidden lg:block">Pagamento </th>
-            <th className="  hidden lg:block">Status</th>
+            <th className="hidden lg:inline">Pagamento </th>
+            <th className="  hidden lg:inline">Status</th>
             <th className="text-end">Ações</th>
           </tr>
         </thead>
@@ -69,8 +69,8 @@ export default async function Servicing() {
               </td>
               <td>{service.serviceprice}</td>
               <td className="mx-1">{service.total}</td>
-              <td className="hidden lg:block" >{service.payment}</td>
-              <td className="hidden lg:block">
+              <td className="hidden lg:inline" >{service.payment}</td>
+              <td className="hidden lg:inline">
                 {service.status === "Realizado" && (
                   <span
                     className="w-3 h-3 rounded-full bg-green-500 inline-block mr-1"
